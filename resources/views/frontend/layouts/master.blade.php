86ebc1f2f6bcb6d41895c976ed037ffa6b2f6799
Egi <egierdian1@gmail.com>
2025-09-02 08:06:03 +0700

    add menu login

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -289,6 +289,14 @@
                                 </svg>
                             </a>
                         </li>
+                        @else
+                        <li>
+                            <a href="{{route('login')}}" class="rounded-circle bg-light p-2 mx-1">
+                                <svg width="24" height="24" viewBox="0 0 24 24">
+                                    <use xlink:href="#user"></use>
+                                </svg>
+                            </a>
+                        </li>
                         @endif
                         <!-- <li>
                             <a href="{{route('frontend.wishlist')}}" class="rounded-circle bg-light p-2 mx-1">
