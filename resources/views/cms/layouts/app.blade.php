334611f6ced3d7c89bd8bb078d7d4a914f7fd90b
Egi <egierdian1@gmail.com>
2025-09-03 14:26:09 +0700

    change title auth

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>Watsha</title>
+	<title>UBakrie</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
 	<link rel="icon" href="{{asset('assets/img/favicon.png')}}" type="image/x-icon"/>
 
