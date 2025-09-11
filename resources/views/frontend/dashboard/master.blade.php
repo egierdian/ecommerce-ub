eb57395c8f09347a8a1c5c0ed7479a3f631fee6b
Egi <egierdian1@gmail.com>
2025-09-11 08:17:55 +0700

    add datatable dashboard customer

--- resources/views/frontend/dashboard/master.blade.php
+++ resources/views/frontend/dashboard/master.blade.php
@@ -1,6 +1,10 @@
 @extends('frontend.layouts.master')
 
 @section('style')
+<!-- DataTables -->
+<link rel="stylesheet" href="{{asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+
 <style>
     .card {
         border: none;
@@ -115,7 +119,14 @@
 @endsection
 
 @section('script')
+<!-- DataTables -->
+<script src="{{asset('assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
 <script>
-    $(document).ready(function() {})
+    $(document).ready(function() {
+        $("#datatableCustomer").DataTable()
+    })
 </script>
 @endsection
