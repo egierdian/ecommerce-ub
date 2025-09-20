84d0c52ef6d8d7ba83b94941c3ceca93a4f8f8e6
egierdian <egierdian1@gmail.com>
2025-09-20 21:54:36 +0700

    versioning css

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -26,7 +26,7 @@
     />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{asset('frontend/css/vendor.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('frontend/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/style.css') }}?v={{ filemtime(public_path('frontend/style.css')) }}">
     <link rel="icon" href="{{asset($webSettings['x_icon']??'')}}" type="image/x-icon" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
